b96baa9902c754ddadc8513ecdae224045475a2c
fumoto3602 <69480713+fumoto3602@users.noreply.github.com>
2020-12-05 17:34:58 +0900

    デスクトップPCに移すために送る

---
+++ 7章/forループ.py
@@ -0,0 +1,70 @@
+# name = 'Ted'
+# for character in name:
+#     print(character)
+# T
+# e
+# d
+
+# shows = ['GOT','Narcos','Vice']
+# for show in shows:
+#     print(show)
+#
+# GOT
+# Narcos
+# Vice
+
+# coms = ('A.Development','Friends','Always Sunny')
+# for show in coms:
+#     print(show)
+#
+# A.Development
+# Friends
+# Always Sunny
+
+# people = {'G.Bluth II':'A. Development',
+#           'Barney':'HIMYM',
+#           'Dennis':'Always Sunny'}
+# for character in people:
+#     print(character)
+#
+# G.Bluth II
+# Barney
+# Dennis
+
+# tv = ['GOT','Narcos','Vice']
+# i = 0
+# for show in tv:
+#     new = tv[i]
+#     new = new.upper()
+#     tv [i] = new
+#     i += 1
+# print(tv)
+#
+# ['GOT', 'NARCOS', 'VICE']
+
+# tv = ['GOT','Narcos','Vice']
+# coms = ['Arrested Development','friends','Always Sunny']
+# all_shows = []
+#
+# for show in tv:
+#     show = show.upper()
+#     all_shows.append(show)
+#
+# for show in coms:
+#     show = show.upper()
+#     all_shows.append(show)
+# print(all_shows)
+# ['GOT', 'NARCOS', 'VICE', 'ARRESTED DEVELOPMENT', 'FRIENDS', 'ALWAYS SUNNY']
+
+
+
+
+
+
+
+
+
+
+
+
+
